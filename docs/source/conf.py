b363b10056f5b4194fc04001dbd35372e82977fa
Filip Wodnicki <filip.wodnicki@gmail.com>
2021-05-11 13:18:38 +0200

    (docs) Configure documentation to run from notebooks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,17 +35,20 @@ release = "0.1.0"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parser",
     "sphinx_rtd_theme",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "myst_nb",
 ]
-# sphinx.ext.ov
 
-# The suffix of source filenames.
-source_suffix = [".md"]
 
-# source_parsers = {".md": "sphinxcontrib.markdown.MarkdownParser"}
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "myst-nb",
+    ".ipynb": "myst-nb",
+    ".myst": "myst-nb",
+}
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -67,3 +70,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+
+# -- Options for myst_nb -------------------------------------------------
+jupyter_execute_notebooks = "off"
--- docs/source/index.md
+++ docs/source/index.md
@@ -1,17 +1,6 @@
 Welcome to Transit + OSMnx's documentation!
 ===========================================
 
-## This is a subheading
-
-### Mode in depth
-
-```{code-block} python
-# Let's go!
-def abc():
-    pass
-```
-
-
 ```{toctree} 
 ---
 maxdepth: 2
@@ -19,10 +8,17 @@ caption: Contents
 ---
 Install instruction <install>
 Usage <usage>
+Project demo <Demo>
 Code reference <reference>
 Advanced Python Concepts <adv_py>
 Next steps <future>
 ```
+
+### Features of this documentation
+1. Autodoc  
+2. Markdown based (using myst library)  
+3. Parsing ipynb notebooks (using myst-nb library)  
+
 
 ```{eval-rst}
 Indices and tables
